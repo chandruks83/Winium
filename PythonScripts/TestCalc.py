9eb580a49dba4cb85e0086b14afb8a1e95af3c44
Chandru <chandruks83@gmail.com>
2019-07-24 20:36:16 +0530

    First commit from pycharm 29-4-2019

--- PythonScripts/TestCalc.py
+++ PythonScripts/TestCalc.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 import time
+import os
+os.startfile(r'C:\Users\Chandru\PycharmProjects\Winium\WiniumDriver\Winium.Desktop.Driver.exe')
 driver = webdriver.Remote(command_executor='http://localhost:9999',
     desired_capabilities={
         "debugConnectToRunningApp": 'false',
         "app": r"C:/windows/system32/calc.exe"
     })
 window = driver.find_element_by_name('Calculator')
-#time.sleep(2)
 window.find_element_by_name('Five').click()
 window.find_element_by_name('Two').click()
 window.find_element_by_name('Plus').click()
@@ -16,3 +17,4 @@ window.find_element_by_name('Equals').click()
 result = window.find_element_by_id('CalculatorResults').get_attribute('Name')
 print(result)
 window.find_element_by_name('Close').click()
+os.system('TASKKILL /F /IM Winium.Desktop.Driver.exe')
